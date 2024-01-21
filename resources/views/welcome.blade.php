30854d1b8146a9529f20fb802fd1e6493f48fa36
akik-hossain <akikhs00@gmail.com>
2024-01-22 00:18:07 +0600

    added paragraph

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,9 @@
 
 <body>
     <h1>Hello Market Pulse ...This is a user friendly E-commerce project</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro quasi reprehenderit dicta molestiae minus
+        eligendi harum et voluptatibus, illo, earum, nulla dolorum minima veritatis. Vitae et earum autem natus
+        molestias.</p>
 </body>
 
 </html>
