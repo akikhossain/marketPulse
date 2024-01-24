97ed49761209401332b1ca3a05189e56d71cf056
akik-hossain <akikhs00@gmail.com>
2024-01-24 20:53:47 +0600

    changed  footer

--- resources/views/Admin/Partial/footer.blade.php
+++ resources/views/Admin/Partial/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
                 <div class="container footer--flex">
                     <div class="footer-start">
-                        <p>2021 © Elegant Dashboard - <a href="https://themewagon.github.io/elegant/elegant-dashboard.com" target="_blank" rel="noopener noreferrer">elegant-dashboard.com</a></p><br>
-                        <p>Distributed by - <a href="https://themewagon.com/" target="_blank" rel="noopener noreferrer">Themewagon</a></p>
+                        <p>2024 © MarketPulse - <a href="https://themewagon.github.io/elegant/elegant-dashboard.com" target="_blank" rel="noopener noreferrer">elegant-dashboard.com</a></p><br>
+                        <p>Distributed by - <a href="https://themewagon.com/" target="_blank" rel="noopener noreferrer">Akik Hossain & Hasib Reja</a></p>
                     </div>
                     <ul class="footer-end">
                         <li><a href="##">About</a></li>
