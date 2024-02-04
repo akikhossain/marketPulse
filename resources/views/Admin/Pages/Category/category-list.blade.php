bfb701f77cc4f3eb137afd36414f1baa4b68fe80
akik-hossain <akikhs00@gmail.com>
2024-02-04 17:32:36 +0600

    create  category form and table list

--- resources/views/Admin/Pages/Category/category-list.blade.php
+++ resources/views/Admin/Pages/Category/category-list.blade.php
@@ -1,4 +1,56 @@
 @extends('Admin.master')
 @section('content')
-<h1 class="text-bg-danger">Hello</h1>
+<section class="main_content dashboard_part" style="margin: 30px">
+    <div class="main_content_iner ">
+        <div class="container-fluid plr_30 body_white_bg pt_30">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="QA_section">
+                        <div class="white_box_tittle list_header">
+                            <h4>Category List</h4>
+                            <div class="box_right d-flex lms_block">
+                                <div class="serach_field_2">
+                                    <div class="search_inner">
+                                        <form Active="#">
+                                            <div class="search_field">
+                                                <input type="text" placeholder="Search content here...">
+                                            </div>
+                                            <button type="submit"> <i class="ti-search"></i> </button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="add_button ms-2">
+                                    <a href="{{ route('category.form') }}" data-bs-toggle="modal"
+                                        data-bs-target="#addcategory" class="btn_1">Add
+                                        New</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="QA_table ">
+
+                            <table class="table lms_table_active">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#ID</th>
+                                        <th scope="col">Category Name</th>
+                                        <th scope="col">Category Description</th>
+                                        <th scope="col">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>ID</td>
+                                        <td>Category name</td>
+                                        <td>Category Description</td>
+                                        <td><a href="#" class="status_btn">Active</a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
